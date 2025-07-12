a75e8b46479dcae8105b422ef3aa05fd7c1d4fbf
ollie <olliedaly1995@gmail.com>
2025-07-12 22:31:34 +0100

    Update upgrade URL to point to backend

--- popup.js
+++ popup.js
@@ -160,7 +160,7 @@ async function handleUpgradeClick() {
         const userId = tokenInfo.sub; // 'sub' is the user ID in Google's token info
 
         // Open upgrade page in new tab (GitHub Pages)
-        const upgradeUrl = `https://olliedaly.github.io/specific-focus-extension/upgrade.html?token=${token}&user_id=${userId}`;
+        const upgradeUrl = `https://specific-focus-backend-1056415616503.europe-west1.run.app/upgrade?token=${token}&user_id=${userId}`;
         chrome.tabs.create({ url: upgradeUrl });
         
     } catch (error) {
